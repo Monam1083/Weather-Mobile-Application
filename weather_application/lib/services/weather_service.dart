5507e115450a66d29bb8495410b7988a30ee792c
Muhammad Monam Majeed <monammajeed1083@gmail.com>
2026-05-11 04:37:45 +0500

    Update weather_service.dart

--- weather_application/lib/services/weather_service.dart
+++ weather_application/lib/services/weather_service.dart
@@ -4,7 +4,7 @@ import 'package:http/http.dart' as http;
 import 'package:weather_application/models/weather_model.dart';
 
 class WeatherService {
- final String apikey = '';  // Add your own API key here
+final String apikey = 'YOUR_OPENWEATHER_API_KEY'; // Get free key from openweathermap.org
 
   Future<Weather> fatchWeather(String cityName) async {
     final url = Uri.parse(
